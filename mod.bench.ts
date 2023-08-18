60863f92c3c6851a2c1b36170571c6603b687ddb
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-08-18 17:19:50 +0800

    Fix benchmark

--- mod.bench.ts
+++ mod.bench.ts
@@ -2,30 +2,48 @@ import rangeIterator from "./mod.ts";
 Deno.bench({
 	name: "0",
 	fn: () => {
-		Array.from(rangeIterator(1n, 9n))
+		for (let iterator of rangeIterator(1n, 9n)) {
+			iterator;
+		}
 	}
 });
 Deno.bench({
 	name: "1",
 	fn: () => {
-		Array.from(rangeIterator(1, 9))
+		for (let iterator of rangeIterator(1, 9)) {
+			iterator;
+		}
 	}
 });
 Deno.bench({
 	name: "2",
 	fn: () => {
-		Array.from(rangeIterator(Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER))
+		for (let iterator of rangeIterator(Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)) {
+			iterator;
+		}
 	}
 });
 Deno.bench({
 	name: "3",
 	fn: () => {
-		Array.from(rangeIterator(Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER, 0.001))
+		for (let iterator of rangeIterator(Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER, 0.001)) {
+			iterator;
+		}
 	}
 });
 Deno.bench({
 	name: "4",
 	fn: () => {
-		Array.from(rangeIterator(String.fromCodePoint(0), String.fromCodePoint(0x10FFFF)))
+		for (let iterator of rangeIterator(String.fromCodePoint(0), String.fromCodePoint(0x10FFFF))) {
+			iterator;
+		}
+	}
+});
+Deno.bench({
+	name: "5",
+	fn: () => {
+		for (let iterator of rangeIterator(BigInt(Number.MIN_SAFE_INTEGER), BigInt(Number.MAX_SAFE_INTEGER))) {
+			iterator;
+		}
 	}
 });
