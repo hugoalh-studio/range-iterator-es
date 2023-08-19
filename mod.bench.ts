b326cad873e9456856bac69c7d2a9c0cbe24269a
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-08-19 12:42:01 +0800

    Update workflow

--- mod.bench.ts
+++ mod.bench.ts
@@ -2,7 +2,7 @@ import rangeIterator from "./mod.ts";
 Deno.bench({
 	name: "1",
 	fn: () => {
-		for (let iterator of rangeIterator(1n, 9n)) {
+		for (let iterator of rangeIterator(1n, 100n)) {
 			iterator;
 		}
 	}
@@ -10,23 +10,7 @@ Deno.bench({
 Deno.bench({
 	name: "2",
 	fn: () => {
-		for (let iterator of rangeIterator(1, 9)) {
-			iterator;
-		}
-	}
-});
-Deno.bench({
-	name: "3",
-	fn: () => {
-		for (let iterator of rangeIterator(Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)) {
-			iterator;
-		}
-	}
-});
-Deno.bench({
-	name: "4",
-	fn: () => {
-		for (let iterator of rangeIterator(Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER, 0.001)) {
+		for (let iterator of rangeIterator(1, 100)) {
 			iterator;
 		}
 	}
@@ -35,14 +19,6 @@ Deno.bench({
 	name: "5",
 	fn: () => {
 		for (let iterator of rangeIterator(String.fromCodePoint(0), String.fromCodePoint(0x10FFFF))) {
-			iterator;
-		}
-	}
-});
-Deno.bench({
-	name: "6",
-	fn: () => {
-		for (let iterator of rangeIterator(BigInt(Number.MIN_SAFE_INTEGER), BigInt(Number.MAX_SAFE_INTEGER))) {
 			iterator;
 		}
 	}
