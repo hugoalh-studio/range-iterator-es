1cb05035b607644dc3e12ef49cdcf0d09f9d5344
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-09-05 11:49:43 +0800

    Update test

--- mod.bench.ts
+++ mod.bench.ts
@@ -1,71 +1,71 @@
 import rangeIterator from "./mod.ts";
-Deno.bench("Number 1~9", () => {
+Deno.bench("Number 1~9", { permissions: "none" }, () => {
 	for (const iterator of rangeIterator(1, 9)) {
 		iterator;
 	}
 });
-Deno.bench("Big Integer 1~9 End Exclusive", () => {
+Deno.bench("Big Integer 1~9 End Exclusive", { permissions: "none" }, () => {
 	for (const iterator of rangeIterator(1n, 9n, { endExclusive: true })) {
 		iterator;
 	}
 });
-Deno.bench("Number 1~9 Step 0.5", () => {
+Deno.bench("Number 1~9 Step 0.5", { permissions: "none" }, () => {
 	for (const iterator of rangeIterator(1, 9, { step: 0.5 })) {
 		iterator;
 	}
 });
-Deno.bench("Character a~z", () => {
+Deno.bench("Character a~z", { permissions: "none" }, () => {
 	for (const iterator of rangeIterator("a", "z")) {
 		iterator;
 	}
 });
-Deno.bench("Number 9~1", () => {
+Deno.bench("Number 9~1", { permissions: "none" }, () => {
 	for (const iterator of rangeIterator(9, 1)) {
 		iterator;
 	}
 });
-Deno.bench("Big Integer 9~1 End Exclusive", () => {
+Deno.bench("Big Integer 9~1 End Exclusive", { permissions: "none" }, () => {
 	for (const iterator of rangeIterator(9n, 1n, { endExclusive: true })) {
 		iterator;
 	}
 });
-Deno.bench("Number 9~1 Step 0.5", () => {
+Deno.bench("Number 9~1 Step 0.5", { permissions: "none" }, () => {
 	for (const iterator of rangeIterator(9, 1, { step: 0.5 })) {
 		iterator;
 	}
 });
-Deno.bench("Character z~a", () => {
+Deno.bench("Character z~a", { permissions: "none" }, () => {
 	for (const iterator of rangeIterator("z", "a")) {
 		iterator;
 	}
 });
 
-Deno.bench("Big Integer 1~100", () => {
+Deno.bench("Big Integer 1~100", { permissions: "none" }, () => {
 	for (const iterator of rangeIterator(1n, 100n)) {
 		iterator;
 	}
 });
-Deno.bench("Number 1~100", () => {
+Deno.bench("Number 1~100", { permissions: "none" }, () => {
 	for (const iterator of rangeIterator(1, 100)) {
 		iterator;
 	}
 });
-Deno.bench("Character CodePoint 0~1114111", () => {
+Deno.bench("Character CodePoint 0~1114111", { permissions: "none" }, () => {
 	for (const iterator of rangeIterator(String.fromCodePoint(0), String.fromCodePoint(0x10FFFF))) {
 		iterator;
 	}
 });
-Deno.bench("Big Integer 100~1", () => {
+Deno.bench("Big Integer 100~1", { permissions: "none" }, () => {
 	for (const iterator of rangeIterator(100n, 1n)) {
 		iterator;
 	}
 });
-Deno.bench("Number 100~1", () => {
+Deno.bench("Number 100~1", { permissions: "none" }, () => {
 	for (const iterator of rangeIterator(100, 1)) {
 		iterator;
 	}
 });
-Deno.bench("Character CodePoint 1114111~0", () => {
+Deno.bench("Character CodePoint 1114111~0", { permissions: "none" }, () => {
 	for (const iterator of rangeIterator(String.fromCodePoint(0x10FFFF), String.fromCodePoint(0))) {
 		iterator;
 	}
--- mod.test.ts
+++ mod.test.ts
@@ -1,26 +1,26 @@
 import { assertEquals } from "https://deno.land/std@0.201.0/assert/assert_equals.ts";
 import rangeIterator from "./mod.ts";
-Deno.test("Number 1~9", () => {
+Deno.test("Number 1~9", { permissions: "none" }, () => {
 	assertEquals(Array.from(rangeIterator(1, 9)), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
 });
-Deno.test("Big Integer 1~9 End Exclusive", () => {
+Deno.test("Big Integer 1~9 End Exclusive", { permissions: "none" }, () => {
 	assertEquals(Array.from(rangeIterator(1n, 9n, { endExclusive: true })), [1n, 2n, 3n, 4n, 5n, 6n, 7n, 8n]);
 });
-Deno.test("Number 1~9 Step 0.5", () => {
+Deno.test("Number 1~9 Step 0.5", { permissions: "none" }, () => {
 	assertEquals(Array.from(rangeIterator(1, 9, { step: 0.5 })), [1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9]);
 });
-Deno.test("Character a~z", () => {
+Deno.test("Character a~z", { permissions: "none" }, () => {
 	assertEquals(Array.from(rangeIterator("a", "z")), ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]);
 });
-Deno.test("Number 9~1", () => {
+Deno.test("Number 9~1", { permissions: "none" }, () => {
 	assertEquals(Array.from(rangeIterator(9, 1)), [9, 8, 7, 6, 5, 4, 3, 2, 1]);
 });
-Deno.test("Big Integer 9~1 End Exclusive", () => {
+Deno.test("Big Integer 9~1 End Exclusive", { permissions: "none" }, () => {
 	assertEquals(Array.from(rangeIterator(9n, 1n, { endExclusive: true })), [9n, 8n, 7n, 6n, 5n, 4n, 3n, 2n]);
 });
-Deno.test("Number 9~1 Step 0.5", () => {
+Deno.test("Number 9~1 Step 0.5", { permissions: "none" }, () => {
 	assertEquals(Array.from(rangeIterator(9, 1, { step: 0.5 })), [9, 8.5, 8, 7.5, 7, 6.5, 6, 5.5, 5, 4.5, 4, 3.5, 3, 2.5, 2, 1.5, 1]);
 });
-Deno.test("Character z~a", () => {
+Deno.test("Character z~a", { permissions: "none" }, () => {
 	assertEquals(Array.from(rangeIterator("z", "a")), ["z", "y", "x", "w", "v", "u", "t", "s", "r", "q", "p", "o", "n", "m", "l", "k", "j", "i", "h", "g", "f", "e", "d", "c", "b", "a"]);
 });
