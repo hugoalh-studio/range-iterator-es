1a640d4e54edd85aef5a1326df5613d06ca92b05
hugoalh <hugoalh@gmail.com>
2024-10-19 16:09:38 +0800

    Update resources

--- .dnt.ts
+++ .dnt.ts
@@ -8,11 +8,11 @@ await invokeDenoNodeJSTransformer({
 		"LICENSE.md",
 		"README.md"
 	],
-	entrypoints: configJSR.exports,
+	entrypoints: configJSR.getExports(),
 	generateDeclarationMap: true,
 	metadata: {
-		name: "@hugoalh/range-iterator",
-		version: configJSR.version,
+		name: configJSR.getName(),
+		version: configJSR.getVersion(),
 		description: "A module to iterate between range.",
 		keywords: [
 			"range",
